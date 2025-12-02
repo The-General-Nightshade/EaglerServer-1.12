2fde34a536de3e2a38d1462b4db6dec243cea554
Namewalker <malaky.colbary@usd467.org>
2025-12-02 23:21:12 +0000

    Updating Server (added warden, working on shopgui)

--- server/plugins/Inject/src/main/java/com/shadowlord/inject/behaviors/Behavior.java
+++
@@ -1,9 +0,0 @@
-package com.shadowlord.inject.behaviors;
-
-import org.bukkit.entity.Entity;
-
-public interface Behavior {
-  void onAttach(Entity entity);
-  void tick(Entity entity);
-  void onRemove(Entity entity);
-}
--- servers/lobby1/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-java -Xms1G -Xmx2G -jar server.jar nogui
